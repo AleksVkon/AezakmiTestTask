98abdb5cafc435cbfbb5079cbcdc2a7712578811
Aleksey Konchyts <konchits.aleksey@gmail.com>
2024-09-24 12:02:29 +0300

    Initial Commit

---
+++ AezakmiTestTask/AezakmiTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AezakmiTestTaskApp.swift
+//  AezakmiTestTask
+//
+//  Created by Aleksey Konchyts on 24.09.24.
+//
+
+import SwiftUI
+
+@main
+struct AezakmiTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
